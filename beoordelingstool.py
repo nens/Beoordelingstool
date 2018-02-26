8bca7847d0118712edfed8260f2546ee876429c8
GeOdin <madeleine.vanwinkel@gmail.com>
2018-02-26 09:34:24 +0100

    Upload the json, don't show NULL but empty strings in table

--- beoordelingstool.py
+++ beoordelingstool.py
@@ -20,11 +20,14 @@
  *                                                                         *
  ***************************************************************************/
 """
+import base64
 import json
 import os.path
+import urllib2
 import re
 
 from PyQt4.QtCore import QCoreApplication
+from PyQt4.QtCore import QPyNullVariant
 from PyQt4.QtCore import QSettings
 from PyQt4.QtCore import Qt
 from PyQt4.QtCore import QTranslator
@@ -33,6 +36,7 @@ from PyQt4.QtGui import QAction
 from PyQt4.QtGui import QFileDialog
 from PyQt4.QtGui import QTableWidgetItem
 from PyQt4.QtGui import QIcon
+import qgis.core
 from qgis.core import QgsExpression
 from qgis.core import QgsFeature
 from qgis.core import QgsFeatureRequest
@@ -356,7 +360,7 @@ class Beoordelingstool:
         fields = layer.dataProvider().fields()
         for f in layer.selectedFeatures():
             self.dockwidget.field_combobox_manholes.setCurrentIndex(self.dockwidget.field_combobox_manholes.findText(str(f["Herstelmaa"]))) if self.dockwidget.field_combobox_manholes.findText(str(f["Herstelmaa"])) else self.dockwidget.field_combobox_manholes.setCurrentIndex(0)
-            self.dockwidget.value_plaintextedit_manholes.setPlainText(str(f["Opmerking"]))
+            self.dockwidget.value_plaintextedit_manholes.setPlainText(f["Opmerking"] if type(f["Opmerking"]) is not QPyNullVariant else "")
             self.dockwidget.tablewidget_manholes.setItem(0, 0, QTableWidgetItem(f["CAA"]))
             self.dockwidget.tablewidget_manholes.setItem(0, 1, QTableWidgetItem(f["CAJ"]))
             self.dockwidget.tablewidget_manholes.setItem(0, 2, QTableWidgetItem(f["CAL"]))
@@ -414,7 +418,7 @@ class Beoordelingstool:
         fields = layer.dataProvider().fields()
         for f in layer.selectedFeatures():
             self.dockwidget.field_combobox_pipes.setCurrentIndex(self.dockwidget.field_combobox_pipes.findText(str(f["Herstelmaa"]))) if self.dockwidget.field_combobox_pipes.findText(str(f["Herstelmaa"])) else self.dockwidget.field_combobox_pipes.setCurrentIndex(0)
-            self.dockwidget.value_plaintextedit_pipes.setPlainText(str(f["Opmerking"]))
+            self.dockwidget.value_plaintextedit_pipes.setPlainText(str(f["Opmerking"]) if type(f["Opmerking"]) is not QPyNullVariant else "")
             self.dockwidget.tablewidget_pipes.setItem(0, 0, QTableWidgetItem(f["AAA"]))
             self.dockwidget.tablewidget_pipes.setItem(0, 1, QTableWidgetItem(f["AAB"]))
             self.dockwidget.tablewidget_pipes.setItem(0, 2, QTableWidgetItem(f["AAD"]))
@@ -501,7 +505,8 @@ class Beoordelingstool:
 
             # Set values
             self.dockwidget.field_combobox_measuring_stations.setCurrentIndex(self.dockwidget.field_combobox_measuring_stations.findText(str(new_feature["Herstelmaa"]))) if self.dockwidget.field_combobox_measuring_stations.findText(str(new_feature["Herstelmaa"])) else self.dockwidget.field_combobox_measuring_stations.setCurrentIndex(0)
-            self.dockwidget.value_plaintextedit_measuring_stations.setPlainText(new_feature["Opmerking"] if new_feature["Opmerking"] else '')
+            opmerking = new_feature["Opmerking"] if new_feature["Opmerking"] and type(new_feature["Opmerking"]) is not QPyNullVariant else ''
+            self.dockwidget.value_plaintextedit_measuring_stations.setPlainText(opmerking)
             self.dockwidget.tablewidget_measuring_stations.setItem(0, 0, QTableWidgetItem(new_feature["PIPE_ID"]))
             self.dockwidget.tablewidget_measuring_stations.setItem(0, 1, QTableWidgetItem(new_feature["A"]))
             self.dockwidget.tablewidget_measuring_stations.setItem(0, 2, QTableWidgetItem(new_feature["B"]))
@@ -526,7 +531,7 @@ class Beoordelingstool:
         fields = layer.dataProvider().fields()
         for f in layer.selectedFeatures():
             self.dockwidget.field_combobox_measuring_stations.setCurrentIndex(self.dockwidget.field_combobox_measuring_stations.findText(str(f["Herstelmaa"]))) if self.dockwidget.field_combobox_measuring_stations.findText(str(f["Herstelmaa"])) else self.dockwidget.field_combobox_measuring_stations.setCurrentIndex(0)
-            self.dockwidget.value_plaintextedit_measuring_stations.setPlainText(str(f["Opmerking"]))
+            self.dockwidget.value_plaintextedit_measuring_stations.setPlainText(str(f["Opmerking"]) if type(f["Opmerking"]) is not QPyNullVariant else "")
             self.dockwidget.tablewidget_measuring_stations.setItem(0, 0, QTableWidgetItem(f["PIPE_ID"]))
             self.dockwidget.tablewidget_measuring_stations.setItem(0, 1, QTableWidgetItem(f["A"]))
             self.dockwidget.tablewidget_measuring_stations.setItem(0, 2, QTableWidgetItem(f["B"]))
@@ -565,7 +570,8 @@ class Beoordelingstool:
                 self.selected_measuring_station_id = measuring_station_id_new
                 # Update Measuring stations tab and tablewidget
                 self.dockwidget.field_combobox_measuring_stations.setCurrentIndex(self.dockwidget.field_combobox_measuring_stations.findText(str(new_feature["Herstelmaa"]))) if self.dockwidget.field_combobox_measuring_stations.findText(str(new_feature["Herstelmaa"])) else self.dockwidget.field_combobox_measuring_stations.setCurrentIndex(0)
-                self.dockwidget.value_plaintextedit_measuring_stations.setPlainText(new_feature["Opmerking"] if new_feature["Opmerking"] else '')
+                opmerking = new_feature["Opmerking"] if new_feature["Opmerking"] and type(new_feature["Opmerking"]) is not QPyNullVariant else ''
+                self.dockwidget.value_plaintextedit_measuring_stations.setPlainText(opmerking)
                 self.dockwidget.tablewidget_measuring_stations.setItem(0, 0, QTableWidgetItem(new_feature["PIPE_ID"]))
                 self.dockwidget.tablewidget_measuring_stations.setItem(0, 1, QTableWidgetItem(new_feature["A"]))
                 self.dockwidget.tablewidget_measuring_stations.setItem(0, 2, QTableWidgetItem(new_feature["B"]))
@@ -596,7 +602,7 @@ class Beoordelingstool:
             layer.setSelectedFeatures([int(self.selected_pipe_id)])
             new_feature = layer.selectedFeatures()[0]
             self.dockwidget.field_combobox_pipes.setCurrentIndex(self.dockwidget.field_combobox_pipes.findText(str(new_feature["Herstelmaa"]))) if self.dockwidget.field_combobox_pipes.findText(str(new_feature["Herstelmaa"])) else self.dockwidget.field_combobox_pipes.setCurrentIndex(0)
-            self.dockwidget.value_plaintextedit_pipes.setPlainText(str(new_feature["Opmerking"]))
+            self.dockwidget.value_plaintextedit_pipes.setPlainText(str(new_feature["Opmerking"]) if type(new_feature["Opmerking"]) is not QPyNullVariant else "")
             self.dockwidget.tablewidget_pipes.setItem(0, 0, QTableWidgetItem(new_feature["AAA"]))
             self.dockwidget.tablewidget_pipes.setItem(0, 1, QTableWidgetItem(new_feature["AAB"]))
             self.dockwidget.tablewidget_pipes.setItem(0, 2, QTableWidgetItem(new_feature["AAD"]))
@@ -670,7 +676,8 @@ class Beoordelingstool:
                 # Update Measuring stations tab and tablewidget
 
                 self.dockwidget.field_combobox_measuring_stations.setCurrentIndex(self.dockwidget.field_combobox_measuring_stations.findText(str(new_feature["Herstelmaa"]))) if self.dockwidget.field_combobox_measuring_stations.findText(str(new_feature["Herstelmaa"])) else self.dockwidget.field_combobox_measuring_stations.setCurrentIndex(0)
-                self.dockwidget.value_plaintextedit_measuring_stations.setPlainText(new_feature["Opmerking"] if new_feature["Opmerking"] else '')
+                opmerking = new_feature["Opmerking"] if new_feature["Opmerking"] and type(new_feature["Opmerking"]) is not QPyNullVariant else ''
+                self.dockwidget.value_plaintextedit_measuring_stations.setPlainText(opmerking)
                 self.dockwidget.tablewidget_measuring_stations.setItem(0, 0, QTableWidgetItem(new_feature["PIPE_ID"]))
                 self.dockwidget.tablewidget_measuring_stations.setItem(0, 1, QTableWidgetItem(new_feature["A"]))
                 self.dockwidget.tablewidget_measuring_stations.setItem(0, 2, QTableWidgetItem(new_feature["B"]))
@@ -709,7 +716,17 @@ class Beoordelingstool:
         If the user data typed in the login dialog is correct, a json
         is created from the shapefiles and uploaded to the server.
         """
+        # Check if the manholes, pipes and measuring_points layers exist
         self.login_dialog_voortgang.show()
+        # manholes_layerList = QgsMapLayerRegistry.instance().mapLayersByName("manholes")
+        # pipes_layerList = QgsMapLayerRegistry.instance().mapLayersByName("pipes")
+        # measuring_stations_layerList = QgsMapLayerRegistry.instance().mapLayersByName("measuring_points")
+        # if manholes_layerList and pipes_layerList and measuring_stations_layerList:
+        #     # Check user login credentials ()
+        #     username = "Aagje_opdr_nemer"
+        #     self.login_dialog_voortgang = BeoordelingstoolLoginDialog()
+        #     self.login_dialog_voortgang.lineedit_username.setText(username)
+        #     self.login_dialog_voortgang.show()
 
     def show_login_dialog_final(self):
         """
@@ -723,33 +740,35 @@ class Beoordelingstool:
     def upload_voortgang(self, user_data):
         """Upload the voortgang (json)."""
         print "voortgang"
-        self.convert_shps_to_json(user_data)
+        review_json = self.convert_shps_to_json()
+        self.upload_json(review_json, user_data)
         # upload json to server (get url from json)
         iface.messageBar().pushMessage("Info", "JSON saved.", level=QgsMessageBar.INFO, duration=0)
 
     def upload_final(self, user_data):
         """Upload the final version (json + zip with shapefiles and qmls)."""
         print "final"
-        self.convert_shps_to_json(user_data)
+        review_json = self.convert_shps_to_json()
+        self.upload_json(review_json, user_data)
         # save zip
-        # upload json and zip to server (get url from json)
+        # upload json and zip to server (get url_zip from json)
         iface.messageBar().pushMessage("Info", "JSON and ZIP? saved.", level=QgsMessageBar.INFO, duration=0)
 
-    def convert_shps_to_json(self, user_data):
+    def convert_shps_to_json(self):
         """
         Convert the manholes, pipes and measuring points shapefiles to a json.
 
         Args:
             (dict) user_data: A dict containing the username and password.
+
+        Returns:
+            (dict) json_: A dict containing the information about the project and the shapefiles.
         """
         # Check if the manholes, pipes and measuring_points layers exist
         manholes_layerList = QgsMapLayerRegistry.instance().mapLayersByName("manholes")
         pipes_layerList = QgsMapLayerRegistry.instance().mapLayersByName("pipes")
         measuring_stations_layerList = QgsMapLayerRegistry.instance().mapLayersByName("measuring_points")
         if manholes_layerList and pipes_layerList and measuring_stations_layerList:
-            # Check user login credentials ()
-            username = user_data["username"]
-            password = user_data["password"]
             # Get directory to save json in
             layer_dir = get_layer_dir(manholes_layerList[0])
             json_path = os.path.join(layer_dir, JSON_NAME)
@@ -765,22 +784,27 @@ class Beoordelingstool:
                     json_project[JSON_KEY_NAME] = data[JSON_KEY_PROJ][JSON_KEY_NAME]
                 else:
                     iface.messageBar().pushMessage("Error", "No project name found.", level=QgsMessageBar.CRITICAL, duration=0)
-                    return
+                    return json_
+                if data[JSON_KEY_PROJ][JSON_KEY_USERNAME]:
+                    json_project[JSON_KEY_USERNAME] = data[JSON_KEY_PROJ][JSON_KEY_USERNAME]
+                else:
+                    iface.messageBar().pushMessage("Error", "No username found.", level=QgsMessageBar.CRITICAL, duration=0)
+                    return json_
                 if data[JSON_KEY_PROJ][JSON_KEY_URL]:
                     json_project[JSON_KEY_URL] = data[JSON_KEY_PROJ][JSON_KEY_URL]
                 else:
                     iface.messageBar().pushMessage("Error", "No project url found.", level=QgsMessageBar.CRITICAL, duration=0)
-                    return
+                    return json_
                 if data[JSON_KEY_PROJ][JSON_KEY_URL_JSON]:
                     json_project[JSON_KEY_URL_JSON] = data[JSON_KEY_PROJ][JSON_KEY_URL_JSON]
                 else:
                     iface.messageBar().pushMessage("Error", "No upload url found for the JSON.", level=QgsMessageBar.CRITICAL, duration=0)
-                    return
+                    return json_
                 if data[JSON_KEY_PROJ][JSON_KEY_URL_ZIP]:
                     json_project[JSON_KEY_URL_ZIP] = data[JSON_KEY_PROJ][JSON_KEY_URL_ZIP]
                 else:
                     iface.messageBar().pushMessage("Error", "No upload url found for the zip-file.", level=QgsMessageBar.CRITICAL, duration=0)
-                    return
+                    return json_
             else:
                 iface.messageBar().pushMessage("Error", "No json found.", level=QgsMessageBar.CRITICAL, duration=0)
                 return
@@ -789,8 +813,47 @@ class Beoordelingstool:
             json_["manholes"] = manholes
             with open("{}".format(json_path), 'w') as outfile:
                 json.dump(json_, outfile, indent=2)
+            return json_
         else:
             iface.messageBar().pushMessage("Warning", "You don't have a manholes, pipes and measuring_points layer.", level=QgsMessageBar.WARNING, duration=0)
+            json_ = {}
+            return json_
+
+    def upload_json(self, review_json, user_data):
+        """Upload a json to the review_json["project"]["url_json"]"""
+        # Check user login credentials ()  # not needed, checked when json is uploaded
+        # username = user_data["username"]
+        # password = user_data["password"]
+        url = review_json["project"]["url_json"]
+        encoded_user = base64.b64encode(user_data)
+        req = urllib2.Request(url, review_json, encoded_user)
+        response = urllib2.urlopen(req)
+        the_page = reponse.read()  # nodig
+        # # GGMN  https://github.com/nens/ggmn-qgis/blob/master/lizard_downloader.py#L534
+        # form = urllib2_upload.MultiPartForm()
+        # form.add_field('title', title)
+        # form.add_field('organisation_id', str(self.selected_organisation))
+        # filename = os.path.basename(tiff_filename)
+        # form.add_file('raster_file', filename, fileHandle=open(tiff_filename, 'rb'))
+
+        # request = urllib2.Request('https://ggmn.un-igrac.org/upload_raster/')
+        # request.add_header('User-agent', 'qgis ggmn uploader')
+        # request.add_header('username', self.username)
+        # request.add_header('password', self.password)
+        # body = str(form)
+        # request.add_header('Content-type', form.get_content_type())
+        # request.add_header('Content-length', len(body))
+        # # print("content-length: %s" % len(body))
+        # request.add_data(body)
+
+        # fd2, logfile = tempfile.mkstemp(prefix="uploadlog", suffix=".txt")
+        # open(logfile, 'w').write(request.get_data())
+        # # print("Printed what we'll send to %s" % logfile)
+
+        # answer = urllib2.urlopen(request).read()
+        # # print(answer)
+        # # print("Uploaded geotiff to the server")
+        # pop_up_info("Uploaded geotiff to the server")
 
 
 def get_layer_dir(layer):
